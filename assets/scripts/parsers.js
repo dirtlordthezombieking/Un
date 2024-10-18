16822b32801bd6a103da097d347cdddd52c0d11c
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-10-18 09:08:53 -0300

    Update parsers.js

--- assets/scripts/parsers.js
+++ assets/scripts/parsers.js
@@ -3,11 +3,26 @@ const parsers=
 	parseConditon(src)
 	{
 		const type=src["type"];
+		const ret={};
 		switch(type)
 		{
 			case "bool":
-				
+				ret.loc=parseDataLocation(src);
 				break;
 		}
+		return ret;
+	},
+	parseDataLocation(src)
+	{
+		const type=src["location"];
+		const ret={};
+		ret["name"]=src["name"];
+		switch(type)
+		{
+			case "save":
+				ret.
+				break;
+		}
+		return ret;
 	}
 }
