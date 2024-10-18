6341159b863b9b31c32e154d7136845ec6983fb5
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-10-18 10:48:23 -0300

    Update formulas.js

--- assets/scripts/formulas.js
+++ assets/scripts/formulas.js
@@ -3,5 +3,13 @@ const formulas=
 	expToLevel(exp)
 	{
 		return Math.floor(Math.sqrt(exp/25));
+	},
+	multiRandomRoll(chance,count)
+	{
+		return choiceMultiRandomRoll(chance,count,Math.random());
+	},
+	choiceMultiRandomRoll(chance,count,randomNumber)
+	{
+		return ((randomNumber**(1-chance))**(2/chance))*count;
 	}
 };
