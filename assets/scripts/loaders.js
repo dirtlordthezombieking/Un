39abcda1f1ecbd2627d70d143b646b5a9c35b0ca
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-10-18 13:57:54 -0300

    Update loaders.js

--- assets/scripts/loaders.js
+++ assets/scripts/loaders.js
@@ -2,6 +2,14 @@ const loaders=
 {
 	loadSound(src)
 	{
-		const ret=document.createElement(src);
+		const ret=document.createElement("audio");
+		ret.src=""+src+".ogg";
+		ret.type="audio/ogg";
+		ret.setAttribute("preload", "auto");
+		ret.setAttribute("controls", "none");
+		ret.loop=false;
+		ret.style.display="none";
+		document.body.appendChild(ret);
+		return ret;
 	}
 }
